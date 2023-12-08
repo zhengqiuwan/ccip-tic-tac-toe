2fa7fca8c2bb0a6140d67551f78470574bdb94a2
秋秋 <973146586@qq.com>
2023-12-08 21:00:55 +0800

    update:style

--- frontend/components/navigation/navbar.jsx
+++ frontend/components/navigation/navbar.jsx
@@ -1,12 +1,28 @@
-import { ConnectButton } from "@rainbow-me/rainbowkit";
+import { ConnectButton,  lightTheme, } from "@rainbow-me/rainbowkit";
+import { RainbowKitProvider, Theme } from '@rainbow-me/rainbowkit';
+
 import styles from "../../styles/Navbar.module.css";
+
+
+
 export default function Navbar() {
+	const myCustomTheme:Theme  = {
+		colors: {	
+		  connectButtonBackground: "pink",	 
+		},
+		
+	  };
 	return (
+		
 		<nav className={styles.navbar}>
-			<a href="https://chainlink.com" target={"_blank"}>
-				{/* <img className={styles.alchemy_logo} src="/img.png" ></img> */}
-			</a>
-			<ConnectButton></ConnectButton>
+			 <a href="https://chainlink.com" target={"_blank"}>
+				{/*<img className={styles.alchemy_logo} src="/img.png" ></img> */}
+			</a> 
+		
+			<ConnectButton className={styles.button} style={{ backgroundColor: "pink" }}>	
+				<RainbowKitProvider coolMode  theme={myCustomTheme}></RainbowKitProvider>
+			</ConnectButton>
+			
 		</nav>
 	);
 }
